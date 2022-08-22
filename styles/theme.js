52b7a6f1ed9d862bb854b2a677213b121260b916
Nihal-a <nihalavulan1@gamil.com>
2022-08-22 23:07:36 +0530

    Set up for styled-component done and added theme colors , button etc.

---
+++ styles/theme.js
@@ -0,0 +1,11 @@
+export const theme = {
+    text : {
+        primary : '#637BFF',
+        secondary : '#641B98',
+        darkText : '#484848',
+        lightText : '#767676'
+    },
+    button : {
+        primary : '#14CC60'
+    },
+}
