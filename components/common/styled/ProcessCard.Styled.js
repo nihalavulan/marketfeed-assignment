ca68fb194052a5f904888c707da409758346128c
Nihal-a <nihalavulan1@gamil.com>
2022-08-25 01:56:35 +0530

    Process section mobile completed

--- components/common/styled/ProcessCard.Styled.js
+++ components/common/styled/ProcessCard.Styled.js
@@ -1,34 +1,56 @@
 import styled from "styled-components";
 
 export const StyledProcessCard = styled.div`
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+  flex-direction: ${({ layout }) => layout};
+  gap: 13rem;
+
+  & > div {
     display: flex;
-    justify-content: space-between;
-    align-items: center;
-    flex-direction: ${({layout}) => layout};
-    gap: 13rem;
+    flex-direction: column;
+    text-align: left;
 
-    & > div{
-        display: flex;
-        flex-direction: column;
-        text-align: left;
-
-        & > span{
-            color: ${({theme}) => theme.text.secondary};
-            text-transform: uppercase;
-            font-size: 16px;
-            letter-spacing: 2px;
-            font-weight: 700;
-        }
-        h3{
-            font-size: 30px;
-            letter-spacing: -0.5px;
-            margin: 1rem 0;
-        }
-        p{
-            font-size: 16px;
-            line-height: 24px;
-            color: ${({theme}) => theme.text.lightText};
-            max-width: 450px;
-        }
+    & > span {
+      color: ${({ theme }) => theme.text.secondary};
+      text-transform: uppercase;
+      font-size: 16px;
+      letter-spacing: 2px;
+      font-weight: 700;
     }
-`
+    h3 {
+      font-size: 30px;
+      letter-spacing: -0.5px;
+      margin: 1rem 0;
+    }
+    p {
+      font-size: 16px;
+      line-height: 24px;
+      color: ${({ theme }) => theme.text.lightText};
+      max-width: 450px;
+    }
+  }
+
+  @media (max-width: ${({ theme }) => theme.isMobile}) {
+    flex-direction: column;
+    gap: 2rem;
+
+    & > div {
+      text-align: center;
+      & > span {
+        font-size: 12px;
+      }
+      h3 {
+        font-size: 20px;
+      }
+      p {
+        font-size: 14px;
+      }
+    }
+
+    img {
+      width: 320px;
+    }
+  }
+`;
--- components/styled/Process.styled.js
+++ components/styled/Process.styled.js
@@ -1,10 +1,24 @@
 import styled from "styled-components";
 
 export const StyledProcessWrapper = styled.div`
-    text-align: center;
-    margin-top: 5rem;
+  text-align: center;
+  margin-top: 5rem;
 
-    & > div{
-        padding: 8rem 7rem;
+  & > div {
+    padding: 8rem 7rem;
+  }
+
+  @media (max-width: ${({ theme }) => theme.isMobile}) {
+    & > h2 {
+      font-size: 32px;
+      line-height: 38px;
+      letter-spacing: -1px;
     }
-`
+    & > div {
+      padding: 2rem;
+      display: flex;
+      flex-direction: column;
+      gap: 2rem;
+    }
+  }
+`;
